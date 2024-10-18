c814d731b295bccf9f1cde6e9a47830eb297cf70
Arne de Bruijn <mail@arnedebruijn.nl>
2024-10-18 20:37:12 +0200

    Remove support message on error

--- RT/Core/Common.c
+++ RT/Core/Common.c
@@ -8,7 +8,7 @@
 #include "Core/Arena.h"
 #include "Core/String.h"
 
-#define SUPPORT_STRING "For support, screenshot this message and visit the #support channel in our discord server:\n https://discord.gg/9dm93hKrnp\nOr create an issue on our GitHub:\nhttps://github.com/BredaUniversityGames/DXX-Raytracer"
+//#define SUPPORT_STRING "For support, screenshot this message and visit the #support channel in our discord server:\n https://discord.gg/9dm93hKrnp\nOr create an issue on our GitHub:\nhttps://github.com/BredaUniversityGames/DXX-Raytracer"
 
 void RT_FATAL_ERROR_(const char *explanation, const char *title, const char *file, int line)
 {
@@ -17,7 +17,7 @@ void RT_FATAL_ERROR_(const char *explanation, const char *title, const char *fil
 
     PathStripPathA(file_stripped);
 
-    char *message = RT_ArenaPrintF(&g_thread_arena, "Location:\n%s:%d\n\nExplanation:\n%s\n\n" SUPPORT_STRING, file_stripped, line, explanation);
+    char *message = RT_ArenaPrintF(&g_thread_arena, "Location:\n%s:%d\n\nExplanation:\n%s", file_stripped, line, explanation);
     MessageBoxA(NULL, message, title, MB_OK|MB_ICONERROR);
 
     __debugbreak(); // If you're using a debugger, now is your chance to debug.
